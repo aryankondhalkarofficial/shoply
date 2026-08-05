b303445d43160587118bbeaf58ca369b3ddff52c
Aryan Kondhalkar <aryankondhalkar52@gmail.com>
2026-08-05 13:15:08 +0530

    Create user model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      minlength: [2, "Name should be at least 2 characters"],
+      maxlength: [50, "Name cannot exceed 50 characters"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password should be at least 8 characters"],
+      maxlength: [64, "Password cannot exceed 64 characters"],
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+    address: {
+      type: String,
+      required: [true, "Address is required"],
+      trim: true,
+    },
+    city: {
+      type: String,
+      required: [true, "City is required"],
+      trim: true,
+    },
+    postalCode: {
+      type: String,
+      required: [true, "Postal code is required"],
+    },
+    state: {
+      type: String,
+      required: [true, "State is required"],
+      trim: true,
+    },
+    country: {
+      type: String,
+      required: [true, "Country is required"],
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
