16cbc236fb42c70a9f59bb0068fa034643574299
Aryan Kondhalkar <aryankondhalkar52@gmail.com>
2026-08-07 15:39:27 +0530

    Create order controllers

---
+++ server/src/controllers/order.controller.js
@@ -0,0 +1,89 @@
+import Order from "../models/order.model.js";
+import serverError from "../utils/server-error.js";
+
+export const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({ user: req.user });
+    if (orders.length === 0) {
+      return res.status(200).json({
+        success: true,
+        message: "No orders made",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      orders,
+    });
+  } catch (error) {
+    return serverError(error, res);
+  }
+};
+
+export const getOrderById = async (req, res) => {
+  try {
+    const order = await Order.findOne({
+      _id: req.params.id,
+      user: req.user,
+    });
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    return serverError(error, res);
+  }
+};
+
+export const createOrder = async (req, res) => {
+  try {
+    const {
+      product,
+      name,
+      image,
+      quantity,
+      amount,
+      totalAmount,
+      address,
+      city,
+      postalCode,
+      state,
+      country,
+    } = req.body;
+
+    const order = await Order.create({
+      user: req.user,
+      items: [
+        {
+          product,
+          name,
+          image,
+          quantity,
+          amount,
+        },
+      ],
+      totalAmount,
+      shippingAddress: {
+        address,
+        city,
+        postalCode,
+        state,
+        country,
+      },
+      status: "Processing",
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Order created",
+      order,
+    });
+  } catch (error) {
+    return serverError(error, res);
+  }
+};
--- server/src/routes/order.route.js
+++ server/src/routes/order.route.js
@@ -1,5 +1,10 @@
 import express from "express";
 import auth from "../middlewares/auth.middleware.js";
+import {
+  getAllOrders,
+  getOrderById,
+  createOrder,
+} from "../controllers/order.controller.js";
 
 const router = express.Router();
 
