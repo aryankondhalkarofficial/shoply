e5e2953714508ad23a1f5ede9af4f3044f2f1783
Aryan Kondhalkar <aryankondhalkar52@gmail.com>
2026-08-07 18:31:05 +0530

    Configure CORS

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,12 @@ import reviewRouter from "./routes/review.route.js";
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
