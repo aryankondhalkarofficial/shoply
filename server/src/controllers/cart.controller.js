657f5c9a39b4483a158bae9d75c656571a7d3247
Aryan Kondhalkar <aryankondhalkar52@gmail.com>
2026-08-07 12:59:08 +0530

    Create cart controllers and update cart routes

---
+++ server/src/controllers/cart.controller.js
@@ -0,0 +1,77 @@
+import Cart from "../models/cart.model.js";
+import serverError from "../utils/server-error.js";
+
+export const getCart = async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ user: req.user });
+    if (!cart) {
+      return res.status(200).json({
+        success: true,
+        message: "Cart is empty",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      cart,
+    });
+  } catch (error) {
+    return serverError(error, res);
+  }
+};
+
+export const addToCart = async (req, res) => {
+  try {
+    const existingCart = await Cart.findOne({ user: req.user });
+    if (existingCart) {
+      return res.status(400).json({
+        success: false,
+        message: "Cart already exists",
+      });
+    }
+    const { product, quantity } = req.body;
+    const cart = await Cart.create({
+      user: req.user,
+      items: [
+        {
+          product,
+          quantity,
+        },
+      ],
+    });
+    return res.status(201).json({
+      success: true,
+      message: "Cart created",
+      cart,
+    });
+  } catch (error) {
+    return serverError(error, res);
+  }
+};
+
+export const updateCart = async (req, res) => {
+  try {
+    const cart = await Cart.findOneAndUpdate(
+      {
+        user: req.user,
+      },
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "No cart found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Cart updated",
+      cart,
+    });
+  } catch (error) {
+    return serverError(error, res);
+  }
+};
--- server/src/routes/cart.route.js
+++ server/src/routes/cart.route.js
@@ -1,4 +1,9 @@
 import express from "express";
+import {
+  getCart,
+  addToCart,
+  updateCart,
+} from "../controllers/cart.controller.js";
 import auth from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -6,8 +11,7 @@ const router = express.Router();
 router.use(auth);
 
 router.get("/", getCart);
-router.post("/", createCart);
-router.patch("/:id", updateCart);
-router.delete("/:id", deleteCart);
+router.post("/", addToCart);
+router.patch("/", updateCart);
 
 export default router;
