53dbc8c36bded69f63b408d549b8336964fcd91d
Aryan Kondhalkar <aryankondhalkar52@gmail.com>
2026-08-07 18:12:49 +0530

    Implement review validations

--- server/src/routes/review.route.js
+++ server/src/routes/review.route.js
@@ -4,11 +4,13 @@ import {
   getReviewsByProduct,
   createReview,
 } from "../controllers/review.controller.js";
+import validate from "../middlewares/validate.middleware.js";
+import { createReviewSchema } from "../validations/review.validation.js";
 
 const router = express.Router();
 
 router.get("/:productId", getReviewsByProduct);
 
-router.post("/:productId", auth, createReview);
+router.post("/:productId", auth, validate(createReviewSchema), createReview);
 
 export default router;
